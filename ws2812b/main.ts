65ba0d215e13b5b56fdd503d72192c08960f357d
zmrobo <JMD84134792github>
2021-06-07 16:44:15 +0800

    1.0.5

---
+++ ws2812b/main.ts
@@ -0,0 +1,10 @@
+namespace ws2812b {
+    //% shim=sendBufferAsm
+    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
+    }
+
+    //% shim=setBufferMode
+    export function setBufferMode(pin: DigitalPin, mode: number) {
+
+    }
+}
